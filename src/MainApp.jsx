d61cb14df87e657e71975df97347a739bcefa07c
Heidi Fryzell <heidi37@users.noreply.github.com>
2025-07-04 09:02:41 -0400

    [TASK} Conditionally render get recipe only option if more than 3 ingredients

--- src/MainApp.jsx
+++ src/MainApp.jsx
@@ -31,13 +31,15 @@ export default function Body() {
         <ul className="ingredients-list" aria-live="polite">
           {ingredientsList}
         </ul>
-        <div className="get-recipe-container">
+        { ingredientsList.length > 3 &&
+          <div className="get-recipe-container">
           <div>
             <h3>Ready for a recipe?</h3>
             <p>Generate a recipe from your list of ingredients.</p>
           </div>
           <button>Get a recipe</button>
         </div>
+        }
       </section>
       }
     </main>
