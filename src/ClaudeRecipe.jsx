1401a2f66840d22fa9c6932ea27108f313ecbf62
Heidi Fryzell <heidi37@users.noreply.github.com>
2025-07-09 08:15:17 -0400

    [TASK] Add aria-live tag to recipe

--- src/ClaudeRecipe.jsx
+++ src/ClaudeRecipe.jsx
@@ -2,7 +2,7 @@ import ReactMarkdown from 'react-markdown'
 
 export default function ClaudeRecipe(props){
   return(
-    <section>
+    <section aria-live="polite">
           {props.recipe.length > 0 && <h2>Chef Claude Recommends:</h2>}
           <ReactMarkdown>{props.recipe}</ReactMarkdown>
     </section>
